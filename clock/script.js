c6fb739ccb56778edb245b6fab70cfb4c9781933
Kabilankavi141 <121872971+Kabilankavi141@users.noreply.github.com>
2023-05-02 06:05:06 +0100

    Milli Seconds Added

--- clock/script.js
+++ clock/script.js
@@ -1,4 +1,5 @@
 let ampm=document.getElementById('ampm')
+let m_seconds=0;
 
 function displayTime(){
     let time= new Date();
@@ -9,13 +10,26 @@ function displayTime(){
             hr=hr-12 
             ampm.innerHTML='PM'
     }
-    document.getElementById('hour').innerHTML=zero(hr) 
-    document.getElementById('min').innerHTML=min
-    document.getElementById('sec').innerHTML=sec
-    
+    document.getElementById('hour').innerHTML=zero(hr);
+    document.getElementById('min').innerHTML=min;
+    document.getElementById('sec').innerHTML=sec;
+    var milli_seconds=document.getElementById("milli");
+    if(m_seconds==0){
+        milli_seconds.innerHTML="00";
+    }
+    else if(m_seconds<=9){
+        milli_seconds.innerHTML="0"+m_seconds;
+    }
+    else{
+        if(m_seconds==100){
+            m_seconds=0;
+        }
+        milli_seconds.innerHTML=m_seconds;   
+    }
+    m_seconds++;
 }
 function zero(num){
-    return num<10?"0"+num:num
+    return num<10?"0"+num:num;
 }
-setInterval(displayTime,1000)
+setInterval(displayTime,10);
 
