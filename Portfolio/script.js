b4e6cecd32cfadbd32b37d7f2ff0e480d2de91ad
KPT <placement@lab.com>
2024-02-28 12:25:48 +0530

    side Bar

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -2,7 +2,8 @@ let nav = document.getElementById("nav");
 let ham = document.getElementById("menu");
 let close = document.getElementById("close");
 let sec = document.getElementById("sec");
-
+let Skills_list = document.getElementById("Skills-list");
+let skills_nav_child = document.getElementById("skills-nav-child");
 
 ham.addEventListener("click",()=>{
     nav.classList.add('nav-show');
@@ -13,4 +14,8 @@ close.addEventListener("click",()=>{
     nav.classList.remove('nav-show');
     ham.classList.remove('menu-slide');
     sec.classList.remove('section-slide');
+});
+Skills_list.addEventListener("click",()=>{
+    skills_nav_child.classList.toggle('dropdownshow');
+    console.log(skills_nav_child);
 })
